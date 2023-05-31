2e990997fd00128296fd59e0e695f77e6599aa1a
김남태 <ryan@thebiz.kr>
2023-05-31 17:59:11 +0900

    test code

--- src/test/kotlin/com/es/es/ElasticsearchRepositoryTests.kt
+++ src/test/kotlin/com/es/es/ElasticsearchRepositoryTests.kt
@@ -2,6 +2,8 @@ package com.es.es
 
 import com.es.es.document.Phone
 import com.es.es.repository.ElasticsearchRepository
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -18,5 +20,21 @@ class ElasticsearchRepositoryTests() {
     fun save() {
         val phone: Phone = Phone(1, "010-0000-0000", "es")
         elasticsearchRepository.save(phone)
+    }
+
+    @Test
+    @DisplayName("조회")
+    fun findById() {
+        val savedPhone: Phone = Phone(1, "010-0000-0000", "es")
+        elasticsearchRepository.save(savedPhone)
+
+        // 조회
+        val searchedPhone = elasticsearchRepository.findById(1).orElseGet(null)
+
+        // 테스트
+        assertNotNull(searchedPhone)
+        assertEquals(savedPhone.id, searchedPhone.id)
+        assertEquals(savedPhone.number, searchedPhone.number)
+        assertEquals(savedPhone.author, searchedPhone.author)
     }
 }
